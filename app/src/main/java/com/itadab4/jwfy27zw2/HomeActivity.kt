6a2aa9517b8e48eab00d70aa4ffe742cf8e4ec6f
Kyocera-KDZA <83953184+Kyocera-KDZA@users.noreply.github.com>
2022-11-19 01:56:26 +0200

    intent commit2

--- app/src/main/java/com/itadab4/jwfy27zw2/HomeActivity.kt
+++ app/src/main/java/com/itadab4/jwfy27zw2/HomeActivity.kt
@@ -2,10 +2,21 @@ package com.itadab4.jwfy27zw2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
+import android.content.Intent
+import android.widget.Button
+
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val btnReg = findViewById<Button>(R.id.btn_register)
+        btnReg.setOnClickListener {
+            val intent = Intent (this,RegisterActivity::class.java).also{
+                startActivity(it)
+            }
+        }
     }
 }
