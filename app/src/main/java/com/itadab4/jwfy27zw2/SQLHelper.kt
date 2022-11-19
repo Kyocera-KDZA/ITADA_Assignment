58c16422dd7b8ea3e3bf71f362d8b9e6f02a1a91
Kyocera-KDZA <83953184+Kyocera-KDZA@users.noreply.github.com>
2022-11-19 16:05:33 +0200

    sql commit

---
+++ app/src/main/java/com/itadab4/jwfy27zw2/SQLHelper.kt
@@ -0,0 +1,15 @@
+package com.itadab4.jwfy27zw2
+
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class SQLHelper : SQLiteOpenHelper() {
+    override fun onCreate(p0: SQLiteDatabase?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ app/src/main/java/com/itadab4/jwfy27zw2/userModel.kt
@@ -0,0 +1,3 @@
+package com.itadab4.jwfy27zw2
+
+class UserModel (var serID:Int, val name:String, val password:String, val role:String)
